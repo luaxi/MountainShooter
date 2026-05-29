a398304efb193e121ff38899470c8f61ae47c31f
Lua <22667586+luaxi@users.noreply.github.com>
2026-05-28 23:33:54 -0300

    Cria o loop principal, conecta as classes Menu e Game

--- code/menu.py
+++ code/menu.py
@@ -1,9 +1,6 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 class Menu:
-    def __init__(self):
-        self.window = None
+    def __init__(self, window):
+        self.window = window
 
     def run(self, ):
         pass
--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-"""
-MountainShooter Main Game Loop
-Esse módulo cria a janela do jogo e mantém o loop principal.
-"""
+from code.game import Game
 
-import pygame
-
-# Inicializa o Pygame
-pygame.init()
-
-# Configurações da janela
-window = pygame.display.set_mode(size=(600, 480))
-
-# Loop principal
-while True:
-    # Checa todos os eventos
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+game = Game()
+game.run()
